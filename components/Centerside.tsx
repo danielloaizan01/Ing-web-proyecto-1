e1aef67188a75366c6b07e38e48395bce8763141
Daniel tu papi <daniel.loaizan@udea.edu.co>
2023-10-03 21:10:01 -0500

    Primer Cambio

---
+++ components/Centerside.tsx
@@ -0,0 +1,15 @@
+import { ButtonLearn } from "@/components/Buttonlearn";
+
+const Centerside = () => {
+  return (
+    <main className='debug bg-gray-950 w-full'>
+    <section><div><ButtonLearn/></div></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <footer></footer>
+  </main>
+  )
+}
+
+export {Centerside};
---
+++ components/Leftsidebar.tsx
@@ -0,0 +1,12 @@
+const Leftsidebar = () => {
+  return (
+    <aside className='debug w-80 p-10 flex flex-col gap-2'>
+        <div className="bg-gray-950 flex flex-col p-2 gap-4 debug">
+          <span>Daniel Loaiza Noreña</span>
+          <span className="text-gray-500">Ingeniero de sistemas</span>
+        </div>
+    </aside>
+  )
+}
+
+export {Leftsidebar};
